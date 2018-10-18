29e936b455febc3b1ca15f4674f313e89b927406
Nathan Plouvier <naplouvi@z4r6p2.le-101.fr>
2018-10-18 19:18:50 +0200

    change main.c

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: naplouvi <naplouvi@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/10/18 13:01:47 by ftourret     #+#   ##    ##    #+#       */
-/*   Updated: 2018/10/18 17:15:57 by naplouvi    ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/10/18 19:17:53 by naplouvi    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -15,11 +15,21 @@
 
 int		main(int argc, char **argv)
 {
-	int	fd;
+	int		fd;
+	int		code;
+	char	*map[26];
+	int		i;
+	int		j;
 
+	i = 0;
 	ft_usage(argc);
 	if ((fd = open(argv[1], O_RDONLY)) == -1)
 		ft_error();
-
+	while ((code = get_next_line(fd, &map[i][j++])))
+	{
+		if (code == -1)
+			ft_error();
+		if (ft_linelen(map[i][j])
+	}
 	return (0);
 }
