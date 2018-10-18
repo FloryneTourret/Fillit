2c8707ed1e81c8b4a25eb677ee41b532ff911f1d
Nathan Plouvier <naplouvi@z4r6p2.le-101.fr>
2018-10-18 15:52:28 +0200

    modif ft_usage et main"

--- ft_usage.c
+++ ft_usage.c
@@ -6,16 +6,16 @@
 /*   By: naplouvi <naplouvi@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/10/18 13:19:32 by naplouvi     #+#   ##    ##    #+#       */
-/*   Updated: 2018/10/18 15:48:37 by naplouvi    ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/10/18 15:51:51 by naplouvi    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
 #include "fillit.h"
 
-void	ft_usage(int ac)
+void	ft_usage(int argc)
 {
-	if (ac == 1)
+	if (argc != 2)
 	{
 		ft_putstr_fd("usage: ./fillit target_file\n", 1);
 		exit;
--- main.c
+++ main.c
@@ -3,10 +3,10 @@
 /*                                                              /             */
 /*   main.c                                           .::    .:/ .      .::   */
 /*                                                 +:+:+   +:    +:  +:+:+    */
-/*   By: ftourret <ftourret@student.le-101.fr>      +:+   +:    +:    +:+     */
+/*   By: naplouvi <naplouvi@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/10/18 13:01:47 by ftourret     #+#   ##    ##    #+#       */
-/*   Updated: 2018/10/18 13:03:40 by ftourret    ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/10/18 15:52:11 by naplouvi    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -15,9 +15,7 @@
 
 int		main(int argc, char **argv)
 {
-	if (argc == 1)
-		ft_putstr_fd("Usage :", 2);
-	else
-		ft_putstr_fd((const char*)argv[1], 1);
+	ft_usage(argc);
+	ft_putstr_fd((const char*)argv[1], 1);
 	return (0);
 }
