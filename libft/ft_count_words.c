32f26de7faeddce2effc0a760628ea6f24344574
Floryne Tourret <ftourret@z1r2p5.le-101.fr>
2018-10-19 16:46:44 +0200

    update count zord

--- libft/ft_count_words.c
+++ libft/ft_count_words.c
@@ -6,7 +6,7 @@
 /*   By: ftourret <ftourret@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/10/05 15:45:26 by ftourret     #+#   ##    ##    #+#       */
-/*   Updated: 2018/10/18 12:42:51 by ftourret    ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/10/19 16:45:57 by ftourret    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -23,7 +23,8 @@ int		ft_count_words(char const *str, char c)
 	while (str[i] != '\0')
 	{
 		if (str[i + 1] == c || str[i + 1] == '\0')
-			count += 1;
+			if (ft_isprint((int)str[i]) && str[i] != c)
+				count += 1;
 		i++;
 	}
 	return (count);
